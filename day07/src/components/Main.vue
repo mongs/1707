cc451fe379d3080de98885331feef32b852bca6e
mongs <fengjun17@outlook.com>
2017-10-28 10:48:43 +0800

    jsonp 1

--- day07/src/components/Main.vue
+++ day07/src/components/Main.vue
@@ -8,11 +8,22 @@
   </div>
 </template>
 <script>
+import { jsonp } from '@/common/js/jsonp'
 export default {
   watch: {
     '$route': (val) => {
       let tableNum = val.params.id
-      console.log(tableNum)
+      const BASE_URL = 'http://api.dagoogle.cn/news/get-news'
+      const DATA = {
+        tableNum: tableNum
+      }
+      const OPTIONS = {
+        param: 'callback'
+      }
+      jsonp(BASE_URL, DATA, OPTIONS)
+        .then(res => {
+          console.log(res)
+        })
     }
   }
 }
